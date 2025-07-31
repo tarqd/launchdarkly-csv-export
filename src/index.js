fa0f87b58e0edbe5716ea94743d6a5e1c209ced1
tarq <ctarquini@launchdarkly.com>
2025-07-31 15:07:48 -0400

    add support for archive checks

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+
+        if (url.pathname === '/oauth/callback') {
+          
+
+        return new Response('Hello Wosrld!');
+    },
+};
